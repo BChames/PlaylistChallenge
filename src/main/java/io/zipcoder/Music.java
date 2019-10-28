5ee8813b0aac6bc4f2d5db18e2e2485e1bd677e3
Brandon Chambers <brandonmchambers36@gmail.com>
2019-10-27 22:35:57 -0400

    Complete lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,24 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        //Get to string specified index for the start index
+        //return a positive number
+        //return the spaces between the songs
+
+
+        for (int i = startIndex; i < playList.length; i++) {
+
+            if (i - playList.length == 1 || i - playList.length / 2 == 1) {
+                startIndex++;
+            }
+
+        }
+        return startIndex;
+
     }
 }
